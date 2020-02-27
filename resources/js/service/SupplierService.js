286a01f9c277613824fad84b66ab2970637ccfd1
rameesac <ramees586@gmail.com>
2020-02-27 16:14:32 +0530

    Create SupplierService.js

---
+++ resources/js/service/SupplierService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export async function get() {
+    return axios.get("/api/supplier").then(res => res.data);
+}
+
+export async function save(supplier) {
+    return axios
+        .post("/api/supplier", { ...supplier })
+        .then(function(response) {
+            return response;
+        })
+        .catch(function(error) {
+            throw error;
+        });
+}
+
+export async function delete(id) {
+    return axios
+        .delete("/api/supplier/" + id)
+        .then(function(response) {
+            return response;
+        })
+        .catch(function(error) {
+            throw error;
+        });
+}
