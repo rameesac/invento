fca3fda10be49cb8ba16dd9182a81cd9347ecca2
rameesac <ramees586@gmail.com>
2020-03-03 18:05:13 +0530

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+user App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
@@ -12,4 +13,12 @@ class Product extends Model
      * @var string
      */
     protected $table = 'product';
+    
+    /**
+     * Get the category that owns the product.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category);
+    }
 }
