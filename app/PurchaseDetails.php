8f770de8379c8bd2746b70c4a7196ad7349144bb
rameesac <ramees586@gmail.com>
2020-03-03 18:08:56 +0530

    Update PurchaseDetails.php

--- app/PurchaseDetails.php
+++ app/PurchaseDetails.php
@@ -2,9 +2,25 @@
 
 namespace App;
 
+use App\Purchase;
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class PurchaseDetails extends Model
 {
-    //
+    /**
+     * Get the purchase that owns the PurchaseDetails.
+     */
+    public function purchase()
+    {
+        return $this->belongsTo(Purchase);
+    }
+    
+     /**
+     * Get the product that owns the PurchaseDetails.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product);
+    }
 }
