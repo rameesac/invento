5d8b70e81df53e726ec046de48d7e2a3ad4c718d
rameesac <ramees586@gmail.com>
2020-03-06 11:44:59 +0530

    Update StockMovementService.js

--- resources/js/service/StockMovementService.js
+++ resources/js/service/StockMovementService.js
@@ -1,1 +1,27 @@
+import axios from 'axios';
 
+export async function get() {
+    return axios.get('/api/stock_movement').then(res => res.data);
+}
+
+export async function save(stock_movement) {
+    return axios
+        .post('/api/stock_movement', { ...stock_movement })
+        .then(function(response) {
+            return response;
+        })
+        .catch(function(error) {
+            throw error;
+        });
+}
+
+export async function destroy(id) {
+    return axios
+        .delete('/api/stock_movement/' + id)
+        .then(function(response) {
+            return response;
+        })
+        .catch(function(error) {
+            throw error;
+        });
+}
