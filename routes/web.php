8bc8943c92b642db481dc6864a0804c8b8aef62d
rameesac <ramees586@gmail.com>
2020-04-05 14:10:59 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@
 
 /* Auth::routes(); */
 
-Route::get('/{path?}', function($path = null){ 
-    return view('index');
- })->where('path', '.*');
+Route::view('/{path?}', 'index')->where('path', '.*');
+
 
